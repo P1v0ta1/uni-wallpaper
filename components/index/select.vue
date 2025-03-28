95175b85cdf0ea57bcad63605434fd6f6d46e4ac
p1v0tal <2531220201@qq.com>
2025-03-28 15:51:23 +0800

    3/28pm

--- components/index/select.vue
+++ components/index/select.vue
@@ -14,7 +14,7 @@
 	   <view class="content">
 		   <scroll-view>
 			   <view class="item" v-for="item in 8">
-				   <image :src="'/static/wallpaper/w' + item + '.jpg'"></image>
+				   <image :src="'/static/wallpaper/w' + item + '.jpg'"mode="aspectfill"></image>
 			   </view>
 		   </scroll-view>
 	   </view>
@@ -25,5 +25,44 @@
 	import commonTitleVue from '../common-title/common-title.vue';
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.select{
+		margin-top: 50rpx;
+		
+		.date{
+			display: flex;
+			align-items: center;
+			
+			.text{
+				margin-left: 5rpx;
+			}
+		}
+	}
+	
+	.content{
+		width: 720rpx;
+		margin-left: 30rpx;
+		margin-top: 30rpx;
+		
+		scroll-view{
+			white-space: nowrap;
+			
+			.item{
+				width: 200rpx;
+				height: 450rpx;
+				display: inline-block;
+				margin-right: 15rpx;
+				
+				image{
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+				
+				&:last-child{
+					margin-right: 30rpx;
+				}
+			}
+		}
+	}
 </style>
