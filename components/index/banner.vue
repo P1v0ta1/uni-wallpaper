c21aa522b3f0cc8a02561852efbe63daa491e938
p1v0tal <2531220201@qq.com>
2025-03-28 10:51:22 +0800

    init

---
+++ components/index/banner.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="banner">
+		<swiper
+			autoplay
+			circular
+			indicator-dots
+			indicator-color="rgba(255,255,255,0.5)"
+			indicator-active-color="#ffffff"
+			>
+			<swiper-item v-for="item in 5">
+				<image :src="'/static/b' + item +'.jpg'"mode="widthFix"></image>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,11 @@
 <template>
-	
+	<view class="homeLayout">
+		<bannerVue />
+	</view>
 </template>
 
 <script setup>
-	
+	import bannerVue from '../../components/index/banner.vue';
 </script>
 
 <style lang="scss">
