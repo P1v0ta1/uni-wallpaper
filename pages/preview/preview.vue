5fe3efd3d76c803ee4b0fc70e1e0662950dcd17b
p1v0tal <2531220201@qq.com>
2025-04-18 09:20:15 +0800

    4/18am

---
+++ pages/preview/preview.vue
@@ -0,0 +1,33 @@
+<template>
+        <view class="previewLayout">
+                <swiper circular>
+                        <swiper-item v-for="item in 5">
+                                <image src="/static/wallpaper/w7.jpg"></image>
+                        </swiper-item>
+                </swiper>
+                
+                
+        </view>
+</template>
+
+<style lang="scss"> 
+        .previewLayout {
+                width: 100%;
+                height: 100vh;
+                position: relative;
+                
+                swiper {
+                        width: 100%;
+                        height: 100%;
+                        
+                        image {
+                                width: 100%;
+                                height: 100%;
+                        }
+                }
+        }
+</style>
+
+<script setup>
+        
+</script>
